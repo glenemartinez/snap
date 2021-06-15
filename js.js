66c6a824785d0cebeb59587aa8f6452877b3d100
Glen Escobar <escobar.glen@gmail.com>
2021-06-14 22:31:33 -0300

    github snap

---
+++ js.js
@@ -0,0 +1,52 @@
+let str1 = "Snap";
+let str2 = "Crackle";
+let str3 = "Prime";
+
+function snapCrackle(maxValue) {
+  let str4 = [];
+  for (let i = 1; i <= maxValue; i++) {
+    if (i % 2 > 0 && i % 5 === 0) {
+      str4[i] = str1 + str2;
+    } else if (i % 5 === 0) {
+      str4[i] = str2;
+    } else if (i % 2 > 0) {
+      str4[i] = str1 + " ";
+    } else if (i % 2 === 0) {
+      str4[i] = i;
+    }
+  }
+  return str4.toString().replace(",", " ");
+}
+
+console.log(snapCrackle(12));
+/*
+function snapCracklePrime(maxValue) {
+  let str4 = [];
+  for (let i = 1; i <= maxValue; i++) {
+    str4.push(maxValue[i]);
+
+   
+	
+		if( i % 2 > 0 ){
+		str4[i] = 'impar'; }
+		
+		else	if(i % 2 > 0 && i % 5 === 0){
+		str4[i] = 'impar 5'; }	
+
+		else if(i % 2 === 0){
+		str4[i] = i; }
+
+		else if( i % 1  === 0 && i % i === 0 ) {
+		str4[i] =  'Primo';}	
+
+		else if( i % 5 === 0){
+		str4[i] = "de5";}
+
+	
+
+
+  }
+  //		document.write(str4.toString().replace(',',' '));
+  console.log(str4);
+}
+snapCracklePrime();*/
